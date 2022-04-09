1024ae69d0a9efab72475d0dbebef690cc9c6850
Hacktone <101423271+Hacktone@users.noreply.github.com>
2022-04-08 23:46:13 -0700

    Update Create_AzureAD_User.ps1

--- Create_AzureAD_User.ps1
+++ Create_AzureAD_User.ps1
@@ -1,3 +1,4 @@
+#Simple PS Command to create a new user in Azure AD
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile ;
 $PasswordProfile.Password = "<Password>" ;
 New-AzureADUser -AccountEnabled $True -DisplayName "Abby Brown" -PasswordProfile $PasswordProfile -MailNickName "AbbyB" -UserPrincipalName "AbbyB@contoso.com"
